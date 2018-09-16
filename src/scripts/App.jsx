f4bcda880f365aa6f026ddcc55c8fa5535c0d429
Elena Karpenko <karp.ol3000@gmail.com>
2018-09-16 09:00:35 +0300

    switch

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Route, Switch } from "react-router-dom";
 
 import Layout from "./component/Layout/Layout";
 import BurgerBuilder from "./containers/burgerBuilder/BurgerBuilder";
@@ -10,9 +11,11 @@ class App extends Component {
     render () {
         return (
             <div>
-                <Layout>
-                    <BurgerBuilder />
-                    <Checkout />
+                <Layout> 
+                    <Switch>                   
+                        <Route path="/checkout" component={Checkout} />
+                        <Route path="/" component={BurgerBuilder} />
+                    </Switch>
                 </Layout>
             </div>
         );
--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import {render} from "react-dom";
+import {HashRouter} from "react-router-dom";
 
 import App from "./App";
 import "../scss/main.scss";
 
-render(
-    <App />, document.getElementById("root")
+const app = (
+    <HashRouter>
+        <App />
+    </HashRouter>
 )
+
+render(app, document.getElementById("root"));
